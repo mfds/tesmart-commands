d5546d211a4f367c2af8d4f468e40cd96fc7aef1
Michele Fiordispina <michele.fiordispina@countingup.com>
2021-12-10 23:40:20 +0000

    refactor

--- tesmart-commands.go
+++ tesmart-commands.go
@@ -1,14 +1,18 @@
 package commands
 
 import (
-	"encoding/hex"
+	"context"
 	"errors"
-	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+	"time"
 )
 
 var (
@@ -23,57 +27,51 @@ var (
 	OUTPUT = []byte{0xAA, 0xBB, 0x03, 0x11} // last two bytes are: input and input+0x16 (checksum probably)
 )
 
-var (
-	Debug = log.New(os.Stdout, "DEBUG: ", 0)
-	Info  = log.New(os.Stdout, "INFO: ", 0)
-	Error = log.New(os.Stderr, "ERROR: ", 0)
-)
+var Debug = log.New(ioutil.Discard, "DEBUG: ", 0)
 
 type tesmartSwitch struct {
-	conn net.Conn
-
-	err       chan error
-	responses chan []byte
-	Responses chan []byte
-	sw        chan bool
+	host          string
+	conn          net.Conn
+	connectionCtx context.Context
+	cancelFunc    context.CancelFunc
+	receiverFunc  func([]byte)
 }
 
-func NewTesmartSwitch(host string) (*tesmartSwitch, error) {
+func NewTesmartSwitch(host string, port string, receiverFunc func([]byte)) (*tesmartSwitch, error) {
 	t := tesmartSwitch{}
 
-	hasDebugFlag := flag.Bool("v", false, "enable debug messages")
-	flag.Parse()
-
-	if hasDebugFlag == nil || !(*hasDebugFlag) {
-		Debug.SetOutput(ioutil.Discard)
+	if _, ok := os.LookupEnv("DEBUG"); ok {
+		Debug.SetOutput(os.Stdout)
 	}
 
-	err := t.connect(host)
+	err := t.connect(host, port)
 	if err != nil {
 		return nil, err
 	}
 
+	t.receiverFunc = receiverFunc
+
 	return &t, nil
 }
 
-func (t *tesmartSwitch) SwitchInput(input int) (int, error) {
+func (t *tesmartSwitch) SwitchInput(input int) error {
 	if input < 1 && input > 16 {
-		return 0, errors.New("invalid input value")
+		return errors.New("invalid input value")
 	}
 
 	command := injectInputToPayload(SWITCH_INPUT, byte(input))
 	t.send(command)
-	return ExtractInput(<-t.responses)
+	return nil
 }
 
-func (t *tesmartSwitch) SetLedTimeout(input int) (int, error) {
+func (t *tesmartSwitch) SetLedTimeout(input int) error {
 	if input < 0 && input > 30 {
-		return 0, errors.New("invalid LED timeout value")
+		return errors.New("invalid LED timeout value")
 	}
 
 	command := injectInputToPayload(GET_CURRENT_INPUT, byte(input))
 	t.send(command)
-	return ExtractInput(<-t.responses)
+	return nil
 }
 
 func (t *tesmartSwitch) MuteBuzzer() error {
@@ -92,52 +90,48 @@ func (t *tesmartSwitch) DisableAutoInputDetection() error {
 	return t.send(DISABLE_AUTO_INPUT_DETECTION)
 }
 
-func (t *tesmartSwitch) GetCurrentInput() (int, error) {
-	t.send(GET_CURRENT_INPUT)
-	return ExtractInput(<-t.responses)
+func (t *tesmartSwitch) SendGetCurrentInput() error {
+	return t.send(GET_CURRENT_INPUT)
 }
 
-func (t *tesmartSwitch) connect(host string) error {
+func (t *tesmartSwitch) connect(host string, port string) error {
 	Debug.Print("Connecting...")
-	conn, err := net.Dial("tcp", host)
+	var d net.Dialer
+
+	dialCtx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	conn, err := d.DialContext(dialCtx, "tcp", host+":"+port)
 	if err != nil {
-		Error.Printf("Failed to dial: %v", err)
+		Debug.Printf("Failed to dial: %v", err)
 		return err
 	}
 
-	t.conn = conn
-	Info.Printf("Connected to: %s", host)
+	ctx, cancel := context.WithCancel(context.Background())
 
-	t.responses = make(chan []byte, 1)
-	t.Responses = make(chan []byte, 1)
-	t.err = make(chan error, 1)
-	t.sw = make(chan bool, 1)
+	t.host = host
+	t.conn = conn
+	t.connectionCtx = ctx
+	t.cancelFunc = cancel
 
 	go t.receiveLoop()
+	go t.checkConnectionLoop()
 
-	go func() {
-		<-t.err
-		Debug.Println("Error detected, reconnecting...")
-		t.conn.Close()
-		go t.connect(host)
-	}()
+	Debug.Printf("Connected to: %s", host+":"+port)
 
 	return nil
 }
 
 func (t *tesmartSwitch) send(command []byte) error {
-	Debug.Printf("Sending: %#v (%s)", command, hex.EncodeToString(command))
+	Debug.Printf("Sending: %s", printHex(command))
 
 	bytesSent, err := t.conn.Write(command)
 	if err != nil {
-		Error.Printf("Failed to send command: %v", err)
+		Debug.Printf("Failed to send command: %v", err)
 		return err
 	}
-	t.sw <- true
 
 	if bytesSent != 6 {
 		err := fmt.Errorf("wrong amount of byte sent: %d. Expected 6", bytesSent)
-		Error.Printf(err.Error())
+		Debug.Printf(err.Error())
 		return err
 	}
 	Debug.Printf("Sent: %d", bytesSent)
@@ -145,29 +139,66 @@ func (t *tesmartSwitch) send(command []byte) error {
 	return nil
 }
 
-func (t *tesmartSwitch) receiveLoop() {
-	Debug.Print("Receive loop start")
+func (t *tesmartSwitch) checkConnectionLoop() {
+	defer func() {
+		t.cancelFunc()
+		t.conn.Close()
+	}()
+
 	for {
-		Debug.Print("Reading message")
-		response := make([]byte, 6)
+		cmd := exec.Command("ping", "-c4", t.host)
 
-		read, err := t.conn.Read(response)
-		Debug.Printf("Read %d bytes: %s", read, hex.Dump(response))
-		if err != nil {
-			Debug.Printf("Failed to read data from socket: %v", err)
-
-			t.err <- err
-			break
+		if err := cmd.Start(); err != nil {
+			log.Fatalf("cmd.Start: %v", err)
 		}
 
+		if err := cmd.Wait(); err != nil {
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					if status.ExitStatus() != 0 {
+						Debug.Printf("Disconnected")
+						return
+					}
+				}
+			} else {
+				log.Fatalf("cmd.Wait: %v", err)
+			}
+		}
+		Debug.Println("PING")
+	}
+}
+
+func (t *tesmartSwitch) receiveLoop() {
+	defer t.conn.Close()
+
+ReadLoop:
+	for {
 		select {
-		case <-t.sw:
-			t.responses <- response
+		case <-t.connectionCtx.Done():
+			return
 		default:
-			t.Responses <- response
+			response := make([]byte, 6)
+			t.conn.SetDeadline(time.Now().Add(200 * time.Millisecond))
+			read, err := t.conn.Read(response)
+
+			if err != nil {
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					continue ReadLoop
+				} else if err != io.EOF {
+					Debug.Printf("Failed to read data from socket: %v", err)
+					return
+				}
+			}
+
+			if read == 0 {
+				return
+			}
+
+			Debug.Printf("Read %d bytes: %s", read, printHex(response))
+
+			t.receiverFunc(response)
 		}
 	}
-	Debug.Print("Receive loop end")
 }
 
 func injectInputToPayload(payload []byte, input byte) []byte {
@@ -185,11 +216,20 @@ func ExtractInput(response []byte) (int, error) {
 }
 
 func isValidOutput(output []byte) bool {
-	Debug.Printf("isValidOutput = %s", hex.EncodeToString(output))
+	Debug.Printf("isValidOutput = %s", printHex(output))
 
-	return output[0] == 0xAA &&
+	return len(output) != 6 &&
+		output[0] == 0xAA &&
 		output[1] == 0xBB &&
 		output[2] == 0x03 &&
 		output[3] == 0x11 &&
 		output[5]-output[4] == 0x16 // checksum?
+}
+
+func printHex(data []byte) (out string) {
+	out = "\033[1D"
+	for _, b := range data {
+		out += strings.ToUpper(fmt.Sprintf("%3.2x", b))
+	}
+	return
 }
